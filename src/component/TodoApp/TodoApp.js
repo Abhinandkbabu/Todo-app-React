b83c76ce587dd82017e1c8fd5abe58da8f60c27c
Abhinand Babu K <abhinandkbabu@gmail.com>
2024-06-26 10:58:42 +0530

    initial Set Up done

---
+++ src/component/TodoApp/TodoApp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './TodoApp.css'
+
+export default class TodoApp extends Component {
+  render() {
+    return (
+      <div className='todo-Container'>
+        
+        
+      </div>
+    )
+  }
+}
